5cbe14a02b49a3fabc1c03988fe134c83586e750
ide1234000 <34786977+ide1234000@users.noreply.github.com>
2019-03-26 04:23:41 -0700

    Update zone_undercity.cpp

--- src/server/scripts/EasternKingdoms/zone_undercity.cpp
+++ src/server/scripts/EasternKingdoms/zone_undercity.cpp
@@ -919,8 +919,8 @@ static Location ThrallSpawn[] =
     { 1270.474f, 350.982f, -65.027f, 0.034f       },
 };
 
-#define GOSSIP_WRYNN      "Reporting for duty, your majesty! Let the assault begin!"
-#define GOSSIP_THRALL     "I am ready, Warchief."
+#define GOSSIP_WRYNN      "报到！陛下，可以开始进攻了！"
+#define GOSSIP_THRALL     "我准备好了，大酋长。"
 
 /*######
 ## ALLIANCE
